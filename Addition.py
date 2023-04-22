9d8fe73008e889852ed97d559597cd87cc2c2316
Abhay Singh <coding4decoding@gmail.com>
2023-04-22 10:15:57 +0530

    Addition Added By Abhay Singh

---
+++ Addition.py
@@ -0,0 +1,8 @@
+                            # This is a Feature of Addition i.e. Sum of two number.
+
+def add(x, y):   # Here we stored our numbers in variables (x and y).
+    
+    return x + y            # Here Process Addition is performed. 
+
+
+         # This function of Addition is contributed by Abhay Singh
