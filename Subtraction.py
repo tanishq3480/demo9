32200d87f10addabfc425beb6e0dc9c31aff1d59
abhay2code <131277417+abhay2code@users.noreply.github.com>
2023-04-24 08:40:28 +0530

    Contributed By Abhay SIngh

--- Subtraction.py
+++ Subtraction.py
@@ -1,4 +1,4 @@
-# This is a Feature of Subtraction i.e. Difference of two number. 
+  # This is a Feature of Subtraction i.e. Difference of two number. 
 
 def subtract(a , b):       # Here we stored our numbers in parameter (a and b).
     return a - b                # Here main Subtraction is performed. 
